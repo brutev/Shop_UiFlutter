be6b421798d865ce01e94b842dc3b4ba474e047d
Vignesh V <vickyviji46@gmail.com>
2022-05-04 12:15:55 +0530

    added Details page

---
+++ lib/screens/details/components/CartCounter.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import '../../../constants.dart';
+
+class CartCounter extends StatefulWidget {
+  const CartCounter({Key? key}) : super(key: key);
+
+  @override
+  State<CartCounter> createState() => _CartCounterState();
+}
+
+class _CartCounterState extends State<CartCounter> {
+  int numOfItems =1;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        buildSizedBox(press: () {
+          if (numOfItems >1){
+            setState(() {
+              numOfItems --;
+            });
+          }
+        }, icon: Icons.remove),
+        Padding(padding: EdgeInsets.symmetric(horizontal: kDefaultPaddin/2),
+          child: Text(numOfItems.toString().padLeft(2,"0"),
+            style: Theme.of(context).textTheme.headline6,),
+        ),
+        buildSizedBox(icon: Icons.add, press: () {
+          setState(() {
+            numOfItems++;
+          });
+        }),
+      ],
+    );
+  }
+
+  SizedBox buildSizedBox({required IconData icon,required void Function()  press}) {
+    return SizedBox(
+        width: 40,
+        height: 32,
+        child:OutlinedButton(
+          style: OutlinedButton.styleFrom(
+            padding: EdgeInsets.zero,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(13),
+            ),
+          ),
+          onPressed: press,
+          child: Icon(icon),
+        )
+    );
+  }
+}
+
+
